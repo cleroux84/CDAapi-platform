8f81ee6b149e4429f27a10cff71e0e019529d7c1
Gael <zzgael@gmail.com>
2021-07-08 11:53:05 +0000

    final fix ?

--- pwa/types/Book.ts
+++ pwa/types/Book.ts
@@ -5,11 +5,7 @@ export class Book {
     _id?: string,
     public name?: string,
     public author?: string,
-<<<<<<< HEAD
-    public datePublished?: Date
-=======
     public datePublished?: string
->>>>>>> e87503e5674cb3828f221b40f149f95448376bf3
   ) {
     this["@id"] = _id;
   }
